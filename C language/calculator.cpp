b1d980933c55f531e5d0f26ed926ce16c2bddfb1
krish <srikantbrokarp@gamil.com>
2025-10-29 22:54:53 -0700

    cprog

---
+++ C language/calculator.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+main() {
+	int num1, num2 ;
+	float num;
+	printf("\n enter two number:");
+	scanf("%d %d",&num1,&num2);
+		printf("\n sum:%d",num1+num2);
+			printf("\n difference:%d",num1-num2);
+				printf("\n product:%d",num1*num2);
+				num=num1/num2;
+					printf("\n div:%f",num);
+					printf("\n modul:%d",num1%num2);
+					
+					
+					
+}
---
+++ C language/power.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main(){
+	int num;
+	printf("\n enter a number:");
+	scanf("%d",&num);
+	printf("\n square:%d",num*num);
+	printf("\n cube:%d",num*num*num);
+}
